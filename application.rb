647a2ae6b32ab2fd30462bf4d43db351ac5a69d5
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-16 15:03:10 +0900

    triadメソッド追加

--- application.rb
+++ application.rb
@@ -1,5 +1,11 @@
+def triad(root)
+  
+end
+
+
+
 scale = ["C", "C# or D♭" , "D", "D# or E♭", "E", "F", "F# or G♭", "G", "G# or A♭", "A", "A# or B♭", "B"]
 
 puts "構成音を知りたいコードを入力してください"
-root = gets.chomp
-puts scale.index(root)
+chord = gets.chomp
+root = scale.index(chord) #root音を数字に変える
