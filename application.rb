b48a2794efcd40efa1d097223419b845012ef170
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-16 18:48:53 +0900

    構成音をわかりやすく変更

--- application.rb
+++ application.rb
@@ -10,13 +10,13 @@ end
 
 
 
-scale = ["C", "C# or D♭" , "D", "D# or E♭", "E", "F", "F# or G♭", "G", "G# or A♭", "A", "A# or B♭", "B"]
+scale = ["C", "C#(D♭)" , "D", "D#(E♭)", "E", "F", "F#(G♭)", "G", "G#(A♭)", "A", "A#(B♭)", "B"]
 puts "構成音を知りたいコードを入力してください"
 chord = gets.chomp
 root = scale.index(chord) #root音を数字に変える
 
 notes = triad(root)
-
 notes = notes.map{|x| scale[x]}
+
 #文字に戻す
-puts "#{chord}の構成音は#{notes}です" 
+puts "#{chord}の構成音は[#{notes.join(",")}]です" 
