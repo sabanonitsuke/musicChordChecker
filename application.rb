856505435fc39bb4b004cdfd8271a71ac4aa3f8d
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-16 21:59:30 +0900

    #や♭が入った場合に１音変える

--- application.rb
+++ application.rb
@@ -4,12 +4,16 @@ def triad(root)
   return [root, third, fifth]
 end
 
-
-
 scale = ["C", "C#(D♭)" , "D", "D#(E♭)", "E", "F", "F#(G♭)", "G", "G#(A♭)", "A", "A#(B♭)", "B"]
+
 puts "構成音を知りたいコードを入力してください"
 chord = gets.chomp
-root = scale.index(chord) #root音を数字に変える
+
+#rootを求める
+code = chord.match(/[A-G]/).to_s
+root = scale.index(code)
+root += 1 if chord.include?("#")
+root -= 1 if chord.include?("♭")
 
 #関数を呼び出す
 notes = triad(root)
