f605af9ce66a265dc363ed4b2acbbdd9b8cc996d
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-16 15:29:32 +0900

    構成音を返す(上限あり)

--- application.rb
+++ application.rb
@@ -1,13 +1,17 @@
 def triad(root)
   third = root + 4
   fifth = root + 7
-  return [root, third, fifth]
+  notes = [root, third, fifth]
+  return notes
 end
 
 
 
 scale = ["C", "C# or D♭" , "D", "D# or E♭", "E", "F", "F# or G♭", "G", "G# or A♭", "A", "A# or B♭", "B"]
-
 puts "構成音を知りたいコードを入力してください"
 chord = gets.chomp
 root = scale.index(chord) #root音を数字に変える
+
+
+notes = triad(root)
+print notes.map{|x| scale[x]}
