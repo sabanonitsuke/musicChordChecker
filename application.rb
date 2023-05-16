2ac15297b88fa06d36dd9e0c48644da2b1c46e6a
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-16 15:42:03 +0900

    構成音を返す

--- application.rb
+++ application.rb
@@ -2,6 +2,9 @@ def triad(root)
   third = root + 4
   fifth = root + 7
   notes = [root, third, fifth]
+  notes = notes.map do |x|
+    x >= 12 ? x - 12 : x
+  end
   return notes
 end
 
@@ -12,6 +15,8 @@ puts "構成音を知りたいコードを入力してください"
 chord = gets.chomp
 root = scale.index(chord) #root音を数字に変える
 
-
 notes = triad(root)
+
+
+#文字に戻す
 print notes.map{|x| scale[x]}
