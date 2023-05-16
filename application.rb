64d4c8a217e11f6714eac92412e1f586f61a144f
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-16 21:45:43 +0900

    関数への切り出しを戻す

--- application.rb
+++ application.rb
@@ -1,11 +1,7 @@
 def triad(root)
   third = root + 4
   fifth = root + 7
-  notes = [root, third, fifth]
-  notes = notes.map do |x|
-    x >= 12 ? x - 12 : x
-  end
-  return notes
+  return [root, third, fifth]
 end
 
 
@@ -15,7 +11,15 @@ puts "構成音を知りたいコードを入力してください"
 chord = gets.chomp
 root = scale.index(chord) #root音を数字に変える
 
+#関数を呼び出す
 notes = triad(root)
+
+#はみ出した音を調整
+notes = notes.map do |x|
+  x >= 12 ? x - 12 : x
+end
+
+#音階を文字に変換
 notes = notes.map{|x| scale[x]}
 
 #文字に戻す
