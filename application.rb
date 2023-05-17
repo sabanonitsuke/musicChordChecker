a413d5741e4155a8d3cbf69068b19271af72d1cc
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-17 20:13:39 +0900

    リファクタリング

--- application.rb
+++ application.rb
@@ -1,15 +1,3 @@
-def major_triad(root)
-  third = root + 4
-  fifth = root + 7
-  return [root, third, fifth]
-end
-
-def minor_triad(root)
-  third = root + 3
-  fifth = root + 7
-  return [root, third, fifth]
-end
-
 def get_root(chord, scale)
   code = chord.match(/[A-G]/).to_s
   root = scale.index(code)
@@ -18,25 +6,33 @@ def get_root(chord, scale)
   return root
 end
 
+def major(root)
+  third = root + 4
+  fifth = root + 7
+  return [root, third, fifth]
+end
+
+def minor(root)
+  third = root + 3
+  fifth = root + 7
+  return [root, third, fifth]
+end
+
 scale = ["C", "C#(D♭)" , "D", "D#(E♭)", "E", "F", "F#(G♭)", "G", "G#(A♭)", "A", "A#(B♭)", "B"]
 chord = gets.chomp
 root = get_root(chord, scale)
 
 #メジャー・マイナー判別
 if chord.include?("m")
-  notes = minor_triad(root)
+  notes = minor(root)
   #短調基準のコード判別
   notes[2] -= 1 if chord.include?("dim")
 else
-  notes = major_triad(root)
+  notes = major(root)
   #長調基準のコードの判別
-  if chord.include?("sus4")
-    notes[1] += 1
-  elsif chord.include?("aug")
-    notes[2] += 1
-  elsif chord.include?("6")
-    notes[2] += 2
-  end
+  notes[1] += 1 if chord.include?("sus4")
+  notes[2] += 1 if chord.include?("aug")
+  notes[2] += 2 if chord.include?("6")
 end
 
 #長調・短調どちらもあり得るコードの判別
@@ -50,14 +46,9 @@ elsif chord.include?("add9")
   notes.insert(1, root + 2)
 end
 
-
-#はみ出した音を調整
 notes = notes.map do |x|
   x >= 12 ? x - 12 : x
 end
 
-#音階を文字に変換
 notes = notes.map{|x| scale[x]}
-
-#アウトプット
 puts "#{chord}の構成音は[#{notes.join(", ")}]" 
